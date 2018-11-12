b01fcd7c38ee82a57bd9588e3b13dcff3ed1b14f
Tigerow <44978227+Tigerow@users.noreply.github.com>
2018-11-12 16:48:42 +0000

    Add files via upload

---
+++ cli/data/config.lua
@@ -0,0 +1,16 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore"
+  },
+  info_text = "ماتادور ورژن 8 \n اپن شده توسط:\n@Tbbreak",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    11111111111111,
+  }
+}
+return _
+end
